68a83406927a6e6f863d45841b8b846c44b19891
Keo Chhey Young <keochheyyoung@gmail.com>
2025-06-19 21:51:17 +0700

    first commit

---
+++ app/Models/Book.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'bookname','author','publisher', //dd the fields you want to allow
+    ];
+}
